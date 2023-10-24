d6e1447cd5cd700e20f0dddb6f7052ac68f2c311
디자인베이스 <136983939+designbasekorea@users.noreply.github.com>
2023-10-25 01:31:55 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/opta-nextjs",
-  assetPrefix: "/opta-nextjs/",
+  basePath: "/designbase",
+  assetPrefix: "/designbase/",
   trailingSlash: true,
 };
 
